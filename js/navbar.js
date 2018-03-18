e09b0ad0b6017d605db65c4ef942791ab40260db
0neStone <its.johannes@yahoo.com>
2018-03-18 20:14:15 +0100

    fixed events hide

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,7 @@
 var currentEvent = document.getElementById('currentEvent');
 currentEvent.innerHTML = 'Ostern';
 currentEvent.style.display = "none";
+var events = Array.from(document.getElementsByClassName('toggleVisible'));
+for (var i = 0; i < events.length; i++) {
+  events[i].style.display = "none";
+}
