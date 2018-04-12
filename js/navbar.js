974d8b5805eb3c45adf07e0e4ba9f3e3b642e88d
0neStone <its.johannes@yahoo.com>
2018-04-12 22:24:24 +0200

    hid currentEvent

--- js/navbar.js
+++ js/navbar.js
@@ -2,6 +2,6 @@ var currentEvent = document.getElementById('currentEvent');
 currentEvent.innerHTML = 'Ostern';
 currentEvent.style.display = "none";
 var events = Array.from(document.getElementsByClassName('toggleVisible'));
-/*for (var i = 0; i < events.length; i++) {
+for (var i = 0; i < events.length; i++) {
   events[i].style.display = "none";
-}*/
+}
