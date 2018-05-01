976e03ec22208651a8b491db52854e2ce79c6f0a
0neStone <its.johannes@yahoo.com>
2018-05-01 21:56:30 +0200

    added more jquery to navbar

--- js/navbar.js
+++ js/navbar.js
@@ -2,6 +2,20 @@
 var eventUpcoming = true;
 var eventName = "Muttertag"
 
+//Mobile
+navOpen = false;
+function RevealNav() {
+  if (navOpen == true) {
+    $("#mobileOptions").css("display", "none");
+    navOpen = false;
+  }
+  else {
+    $("#mobileOptions").css("display", "block");
+    navOpen = true;
+  }
+}
+
+//Events
 if (eventUpcoming == true) {
   $(".currentEvent").html(eventName);
   $(".currentEvent").css("display", "block");
