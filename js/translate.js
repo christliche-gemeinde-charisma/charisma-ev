37eedb1a7f0e98f5f0b3e21d0dabaf25f3f188a9
0neStone <its.johannes@yahoo.com>
2018-08-06 15:32:38 +0200

    wrote jquery for translate buttons

--- js/translate.js
+++ js/translate.js
@@ -6,7 +6,7 @@ const spanish_h2 = "Hasta 20 diferentes Naciones visitan regularmente nuestro cu
 const spanish = "Por esa razón ofrecemos una traducción simultánea durante todo el evento. Por medio de Auriculares pueden nuestros invitados participar. Momentáneamente les ofrecemos una traducción en <strong>español</strong> , <strong>portugués</strong>, <strong>inglés</strong>  y <strong>francés </strong> <br> </p>";
 
 const portugese_h2 = "Até 20 diferentes nações visitem nosso culto cada semana.";
-const portugese = "Por isso oferecemos tradução simultanea durante o culto inteiro. <br> Todos podem participar do culto através de fones de ouvido<br> No momento oferecemos tradução em <strong>espanhol</strong>, <strong>portugués</strong>, <strong>inglês</strong> e <strong>francês</strong>.";
+const portugese = "Por isso oferecemos tradução simultanea durante o culto inteiro. <br> Todos podem participar do culto através de fones de ouvido<br> No momento oferecemos tradução em <strong>espanhol</strong>, <strong>português</strong>, <strong>inglês</strong> e <strong>francês</strong>.";
 
 const german_h2 = "Bis zu 20 verschiedene Nationen besuchen regelmäßig unseren Gottesdienst.";
 const german = "Deshalb bieten wir simultane Übersetzung während der gesamten Veranstaltung an. <br> Mit Kopfhörern können unsere Besucher in ihrer Sprache teilnehmen.<br> Momentan bieten wir <strong>Spanisch</strong>, <strong>Portugiesisch</strong>, <strong>Englisch</strong> und <strong>Französisch</strong> Übersetzung an.";
@@ -15,3 +15,27 @@ const french_h2 = "Des quatre coins du monde, des personnes viennent régulière
 const french = "C'est pour cela que nous avons mus à votre disposition une traduction pour que vous puissiez suivre le culte a l'aide de casque. A l'heure actuel on propose comme traduction du <strong>espagnol</strong>, <strong>portugais</strong>, <strong>anglais</strong> et <strong>français</strong>.";
 
 //Event Handlers
+$("#translate_es").click(function () {
+  $(".main_headline").html("Traducción");
+  $("#translate_h2").html(spanish_h2);
+  $("#translate_p").html(spanish);
+})
+$("#translate_pt").click(function () {
+  $(".main_headline").html("Tradução");
+  $("#translate_h2").html(portugese_h2);
+  $("#translate_p").html(portugese);
+})
+$("#translate_eng").click(function () {
+  $(".main_headline").html("Translation");
+  $("#translate_h2").html(english_h2);
+  $("#translate_p").html(english);
+})
+$("#translate_fr").click(function () {
+  $(".main_headline").html("Traduction");
+  $("#translate_h2").html(french_h2);
+  $("#translate_p").html(french);
+})
+$("#translate_de").click(function () {
+  $("#translate_h2").html(german_h2);
+  $("#translate_p").html(german);
+})
