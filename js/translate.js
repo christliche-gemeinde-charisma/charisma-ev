b70d1647a2616ebbf8ba83979d9fe2e9408f4862
0neStone <its.johannes@yahoo.com>
2018-08-06 15:11:04 +0200

    added js file and removed html files

---
+++ js/translate.js
@@ -0,0 +1,17 @@
+// Variables for txt
+const english_h2 = "Up to 20 different nations visit our Service on a regular basis.";
+const english = "Because of this we offer simultaneous translation throughout the whole Sermon. <br> With headphones our guests are able to participate in their own Language. <br> Currently we offer translations in <strong>Spanish</strong>, <strong>Portugese</strong>, <strong>English</strong>, and <strong>French</strong>.";
+
+const spanish_h2 = "Hasta 20 diferentes Naciones visitan regularmente nuestro culto.";
+const spanish = "Por esa razón ofrecemos una traducción simultánea durante todo el evento. Por medio de Auriculares pueden nuestros invitados participar. Momentáneamente les ofrecemos una traducción en <strong>español</strong> , <strong>portugués</strong>, <strong>inglés</strong>  y <strong>francés </strong> <br> </p>";
+
+const portugese_h2 = "Até 20 diferentes nações visitem nosso culto cada semana.";
+const portugese = "Por isso oferecemos tradução simultanea durante o culto inteiro. <br> Todos podem participar do culto através de fones de ouvido<br> No momento oferecemos tradução em <strong>espanhol</strong>, <strong>portugués</strong>, <strong>inglês</strong> e <strong>francês</strong>.";
+
+const german_h2 = "Bis zu 20 verschiedene Nationen besuchen regelmäßig unseren Gottesdienst.";
+const german = "Deshalb bieten wir simultane Übersetzung während der gesamten Veranstaltung an. <br> Mit Kopfhörern können unsere Besucher in ihrer Sprache teilnehmen.<br> Momentan bieten wir <strong>Spanisch</strong>, <strong>Portugiesisch</strong>, <strong>Englisch</strong> und <strong>Französisch</strong> Übersetzung an.";
+
+const french_h2 = "Des quatre coins du monde, des personnes viennent régulièrement assister a notre culte.";
+const french = "C'est pour cela que nous avons mus à votre disposition une traduction pour que vous puissiez suivre le culte a l'aide de casque. A l'heure actuel on propose comme traduction du <strong>espagnol</strong>, <strong>portugais</strong>, <strong>anglais</strong> et <strong>français</strong>.";
+
+//Event Handlers
