33c1564eda0587a14e32a893ae3594b0e6cc64a4
0neStone <its.johannes@yahoo.com>
2019-01-07 19:29:11 +0100

    added navbar template to all subsites

--- ch_website/main_website/urls.py
+++ ch_website/main_website/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('vision', views.vision, name='vision'),
 ]
--- ch_website/main_website/views.py
+++ ch_website/main_website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main_website/index.html')
+
+def vision(request):
+    return render(request, 'main_website/pages/vision.html')
