cb220c4d24dbef89f18eddd594e92c0abb27ef9f
0neStone <its.johannes@yahoo.com>
2019-01-08 10:47:20 +0100

    finished main_website conversion

--- ch_website/main_website/urls.py
+++ ch_website/main_website/urls.py
@@ -2,6 +2,33 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Homepage
     path('', views.index, name='index'),
+
+    # Legal notice
+    path('impressum', views.legal_notice, name='impressum'),
+
+    # Church Service
+    path('worship', views.worship, name='worship'),
+    path('supergemeinde', views.sunday_school, name='sunday_school'),
+    path('uebersetzung', views.translation, name='translation'),
+
+    # Events (TODO: create app)
+    path('kalender', views.calendar, name='calendar'),
+    path('event', views.event, name='event'),
+
+    # For You
+    path('fg', views.home_cells, name='family_groups'),
+    path('ng', views.youth_group, name='youth_group'),
+    path('cloob', views.teen_group, name='teen_group'),
+
+    # About Us
+    path('cloob', views.history, name='history'),
+    path('pastors', views.pastors, name='pastors'),
     path('vision', views.vision, name='vision'),
+
+    # More
+    path('cafeteria', views.cafeteria, name='cafeteria'),
+    path('buecher', views.bookstore, name='bookstore'),
+    path('musikschule', views.music_school, name='music_school'),
 ]
--- ch_website/main_website/views.py
+++ ch_website/main_website/views.py
@@ -1,8 +1,51 @@
 from django.shortcuts import render
 
-# Create your views here.
+'''
+Here you can use the same views but replace the templates with those of your own church
+'''
+
+# Homepage
 def index(request):
     return render(request, 'main_website/index.html')
 
+# Legal notice
+def legal_notice(request):
+    return render(request, 'main_website/pages/impressum.html')
+
+# Church Service
+def worship(request):
+    return render(request, 'main_website/pages/lobpreis.html')
+def sunday_school(request):
+    return render(request, 'main_website/pages/supergemeinde')
+def translation(request):
+    return render(request, 'main_website/pages/uebersetzung.html')
+
+# Events (TODO: create app)
+def calendar(request):
+    return render(request, 'main_website/pages/kalender.html')
+def event(request):
+    return render(request, 'main_website/pages/event.html')
+
+# For You
+def home_cells(request):
+    return render(request, 'main_website/pages/fg.html')
+def youth_group(request):
+    return render(request, 'main_website/pages/ng.html')
+def teen_group(request):
+    return render(request, 'main_website/pages/cloeoeb.html')
+
+# About Us
+def history(request):
+    return render(request, 'main_website/pages/geschichte.html')
+def pastors(request):
+    return render(request, 'main_website/pages/pastoren.html')
 def vision(request):
     return render(request, 'main_website/pages/vision.html')
+
+# More
+def cafeteria(request):
+    return render(request, 'main_website/pages/cafeteria.html')
+def bookstore(request):
+    return render(request, 'main_website/pages/buechertisch.html')
+def music_school(request):
+    return render(request, 'main_website/pages/musikschule.html')
